c88ac275b0f3f88d1aa5c92965c0f27518cc2468
Matthias Cullmann <matthias.cullmann@baloise.ch>
2018-02-14 15:43:00 +0100

    refactoring

--- modul/leistungslohn/getAddresses.php
+++ modul/leistungslohn/getAddresses.php
@@ -1,58 +1,45 @@
 <?php
 
-function RecurseXML($xml,$parent="")
-{
-    $child_count = 0;
-    foreach($xml as $key=>$value)
-    {
-        $child_count++;
-        if(RecurseXML($value,$parent.".".$key) == 0)  // no childern, aka "leaf node"
-        {
-            print($parent . "." . (string)$key . " = " . (string)$value . "<BR>\n");
-        }
-    }
-    return $child_count;
-} 
+function formatAddress($partnerEntry){
+    return  trim ($partnerEntry->vorname.' '.$partnerEntry->name."\n".
+        $partnerEntry->domizil->strasse." ".$partnerEntry->domizil->hausnummer."\n".
+        $partnerEntry->domizil->plz." ".$partnerEntry->domizil->ort);
+}
 
-$bkey = 'B028178';
+function loadPerson($bkey) {
+    $url = 'https://osb.bvch.ch/sales/partner/service/basicAuth/PartnerService/v2_2';
+    $body = '
+    <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:v2="http://www.baloise.ch/sales/partner/service/PartnerService/v2_2">
+       <soapenv:Header/>
+       <soapenv:Body>
+          <v2:findPartnerByKey>
+             <v2:cid>cashCal-getAdress-'.time().'</v2:cid>
+             <v2:query>
+                <v2:keyTyp>10</v2:keyTyp>
+                <v2:key>0'.substr($bkey, -6).'</v2:key>
+             </v2:query>
+          </v2:findPartnerByKey>
+       </soapenv:Body>
+    </soapenv:Envelope>
+    ';
+    
+    $opts = array('http' =>
+        array(
+            'method'  => 'POST',
+            'header'  => array("Authorization: Basic AUTH_PARSYS",
+                "Content-Type: text/xml"
+            ),
+            'content' => $body,
+            'timeout' => 15
+        )
+    );
+    
+    $context  = stream_context_create($opts);
+    $result = file_get_contents($url, false, $context);
+    $result = str_replace("S:","",$result);
+    $result = str_replace("ns2:","", $result);
+    $xml = new SimpleXMLElement($result);
+    return $xml->Body->findPartnerByKeyResponse->response->partner->entry;
+}
 
-$url = 'https://osb.bvch.ch/sales/partner/service/basicAuth/PartnerService/v2_2';
-$body = '
-<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:v2="http://www.baloise.ch/sales/partner/service/PartnerService/v2_2">
-   <soapenv:Header/>
-   <soapenv:Body>
-      <v2:findPartnerByKey>
-         <v2:cid>cashCal-getAdress-'.time().'</v2:cid>
-         <v2:query>
-            <v2:keyTyp>10</v2:keyTyp>
-            <v2:key>0'.substr($bkey, -6).'</v2:key>
-         </v2:query>
-      </v2:findPartnerByKey>
-   </soapenv:Body>
-</soapenv:Envelope>
-';
-
-echo $body;
-
-$opts = array('http' =>
-    array(
-        'method'  => 'POST',
-        'header'  => array("Authorization: Basic YjAyODE3ODpiYWxtYXQxMA==" ,
-            "Content-Type: text/xml"
-        ),
-        'content' => $body,
-        'timeout' => 60
-    )
-);
-
-$context  = stream_context_create($opts);
-$result = file_get_contents($url, false, $context);
-
-
-if ($result === FALSE) { /* Handle error */ }
-
-$result = str_replace("S:","",$result);
-$result = str_replace("ns2:","", $result);
-$xml = new SimpleXMLElement($result);
-RecurseXML($xml);
 ?>
