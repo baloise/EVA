21c7705145df7d285c773761b236dc95fa8301a5
eliareutlinger <elia.reutlinger@gibmit.ch>
2018-02-20 22:22:11 +0100

    Style-Path Changes

--- index.php
+++ index.php
@@ -15,23 +15,23 @@
 
     <!-- Custom styles for this template -->
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <link href="css/baletStyles.css" rel="stylesheet">
+    <link href="css/evaStyles.css" rel="stylesheet">
 
   </head>
 
-  
+
   <body>
     <div class="loadScreen">
       <span class="helper"></span><img class="img-responsive" id="loadingImg" src="img/loading.gif"/>
     </div>
-	
+
     <div id="pageContents" style="opacity: 0;">
-        
+
         <?php
             include("database/connect.php");
             include("modul/session/session.php");
         ?>
-        
+
         <!-- Navigation -->
         <div id="naviLink">
             <nav class="navbar navbar-expand-lg navbar-inverse bg-color fixed-top" id="slideMe" style="display: none;">
@@ -44,17 +44,17 @@
               </button>
               <div class="collapse navbar-collapse" id="navbarResponsive">
                       <ul class="navbar-nav ml-auto">
-                          
+
                           <?php
-                              
+
                               $userID = ($mysqli->query("SELECT ID FROM tb_user WHERE bKey = '$session_username'")->fetch_assoc());
-                              
+
                               $sql1 = "SELECT mg.ID, mm.file_path, mm.title FROM tb_ind_nav AS mg INNER JOIN tb_modul AS mm ON mm.ID = mg.tb_modul_ID WHERE mg.tb_user_ID = " . $userID['ID'] . " ORDER BY mg.position";
-                              
+
                               $result = $mysqli->query($sql1);
-                              
+
                               if (isset($result) && $result->num_rows > 0) {
-                                  
+
                                   while($row = $result->fetch_assoc()) {
                                       $link = '
                                       <li class="nav-item">
@@ -62,46 +62,46 @@
                                       </li>
                                       ';
                                       echo $link;
-                                      
+
                                   }
-                      
+
                               } else {
-                      
+
                       $link = '
                                   <li class="nav-item" id="editNavLink">
                                       <a class="nav-link" href="modul/settings/settings.php">'. $translate["Navigation bearbeiten"].'</a>
                                   </li>
                                   ';
                                   echo $link;
-                                  
+
                               }
-                              
-                              
-                          
+
+
+
                           ?>
-                          
+
                 </ul>
               </div>
             </div>
-          </nav>  
+          </nav>
         </div>
-  
+
       <!-- Page Content -->
       <div class="container">
 		<div class="row">
 			<div class="col-lg-1"></div>
 			<div class="col-lg-10">
 				<div page="<?php if(isset($_SESSION["user"]["currentPath"])){ echo $_SESSION["user"]["currentPath"]; } else { echo "modul/dashboard/dashboard.php";} ?>" id="pageContent">
-			  
+
 				</div>
 			</div>
 		</div>
       </div>
       <!-- /.container -->
-      
+
       <footer class="footer" id="slideMeFoot" style="display: none;">
         <div class="container">
-            <a class="foot-link" href="modul/settings/settings.php"><?php echo $translate["Einstellungen"] ?></a><i class="text-muted"> | <?php echo $_SESSION["user"]['username']; ?></i><span class="text-muted">©<a href="https://eliareutlinger.ch"> Elia Reutlinger</a> | 2018 | BALET <a href="https://github.com/baloise/BALET">v.1.0</a></span>
+            <a class="foot-link" href="modul/settings/settings.php"><?php echo $translate["Einstellungen"] ?></a><i class="text-muted"> | <?php echo $_SESSION["user"]['username']; ?></i><span class="text-muted">©<a href="https://eliareutlinger.ch"> Elia Reutlinger</a> | 2018 | EVA <a href="https://github.com/baloise/BALET">v.1.0</a></span>
         </div>
       </footer>
     </div>
@@ -114,7 +114,7 @@
     <!-- Own JS -->
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.19.1/moment.min.js"></script>
     <script src="js/index.js"></script>
-    
+
   </body>
 
 </html>
