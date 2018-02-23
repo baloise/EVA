b2e2c358c8d8f34953b480b4a2a8d8dde2308366
eliareutlinger <elia.reutlinger@gibmit.ch>
2018-02-23 20:45:13 +0100

    Nothiing Changed
    Nothiing Changed

--- database/connect.php
+++ database/connect.php
@@ -4,7 +4,7 @@
 	$_db_database = getenv('APP_DB_NAME');
 	$_db_username = getenv('APP_DB_USER');
 	$_db_passwort = getenv('APP_DB_PASS');
-
+	
     $mysqli = mysqli_connect($_db_host, $_db_username, $_db_passwort, $_db_database);
 
     if (!$mysqli) {
