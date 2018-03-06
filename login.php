138eda7e33caa48c998aacb781e5058a000ca898
Elia Reutlinger <elia.reutlinger@gibmit.ch>
2018-03-06 11:11:13 +0100

    testing
    testing

--- login.php
+++ login.php
@@ -4,8 +4,20 @@
 <b>HEADERS:</b><br /><br />
 <?php
 
+if (!function_exists('getallheaders')) {
+    function getallheaders() {
+           $headers = [];
+       foreach ($_SERVER as $name => $value) {
+           if (substr($name, 0, 5) == 'HTTP_') {
+               $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+           }
+       }
+       return $headers;
+    }
+}
+
 foreach (getallheaders() as $name => $value) {
-    echo "$name: $value\n";
+    echo "($name => $value);\n";
 }
 
 if(isset($test)){
