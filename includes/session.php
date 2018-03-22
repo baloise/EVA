b8e685090cae7f8bec138e33a113f30fd76147a6
Elia Reutlinger <elia.reutlinger@gibmit.ch>
2018-03-22 16:02:11 +0100

    bug fixes

--- includes/session.php
+++ includes/session.php
@@ -4,26 +4,35 @@
         session_start();
     }
 
-    session_regenerate_id();
-
-    if(isset($_SESSION['user']['username'])  && isset($_SESSION['user']['id']) && isset($_SESSION['user']['usergroup']) && isset($_SESSION['user']['language']) && isset($_SESSION['translations']) && isset($_SESSION['appinfo'])){
-
-        $session_username = $_SESSION['user']['username'];
-        $session_userid = $_SESSION['user']['id'];
-        $session_usergroup = $_SESSION['user']['usergroup'];
-        $session_language =$_SESSION['user']['language'];
-        $translate = $_SESSION['translations'];
-        $session_semesterid = $_SESSION['user']['semester'];
-
-        $session_appinfo = $_SESSION['appinfo'];
-
+    if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 1800)) {
+        session_unset();
+        session_destroy();
     } else {
-        header("Location: ./login.php");
-        exit();
-    }
 
-    if($session_usergroup != 5 && $session_usergroup != 1 && $session_usergroup != 2 && $session_usergroup != 3 && $session_usergroup != 4){
-        die("Sie haben keine Berechtigungen zu diesem Modul");
+        session_regenerate_id();
+
+        $_SESSION['LAST_ACTIVITY'] = time();
+
+        if(isset($_SESSION['user']['username'])  && isset($_SESSION['user']['id']) && isset($_SESSION['user']['usergroup']) && isset($_SESSION['user']['language']) && isset($_SESSION['translations']) && isset($_SESSION['appinfo'])){
+
+            $session_username = $_SESSION['user']['username'];
+            $session_userid = $_SESSION['user']['id'];
+            $session_usergroup = $_SESSION['user']['usergroup'];
+            $session_language =$_SESSION['user']['language'];
+            $translate = $_SESSION['translations'];
+            $session_semesterid = $_SESSION['user']['semester'];
+
+            $session_appinfo = $_SESSION['appinfo'];
+
+        } else {
+            header("Location: ./login.php");
+            exit();
+        }
+
+        if($session_usergroup != 5 && $session_usergroup != 1 && $session_usergroup != 2 && $session_usergroup != 3 && $session_usergroup != 4){
+            die("Sie haben keine Berechtigungen zu diesem Modul");
+        }
+
     }
 
 ?>
--- login.php
+++ login.php
@@ -13,19 +13,67 @@
 
 <?php
 
-    if(isset($_GET['error'])){
-        if ($_GET['error'] == "user"){
-            echo "<br/><br/><strong>Dieser B-Key existiert nicht in der Datenbank.</strong>";
-            die();
-        }
-        if ($_GET['error'] == "userdeleted"){
-            echo "<br/><br/><strong>Dieser B-Key wurde gelöscht/deaktiviert.</strong>";
-            die();
-        }
+    ini_set('display_errors', 1);
+
+    if (isset($_POST['username'])){
+        include("auth.php?loginType=normal");
     }
 
-    if (isset($message['error'])){
-        echo $message['error'];
+    //Error ausgeben, falls einer übergeben wurde
+    function error($reason){
+
+        echo '
+        <head>
+            <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+            <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+            <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+        </head>
+        <body>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="error-template">
+                            <h1>
+                                Oops!</h1>
+                            <h2>
+                                There was an Error:</h2>
+                            <div class="error-details">
+                                '.$reason.'
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </body>
+        ';
+    }
+
+    //Prüfen, ob Token verfügbar ist
+    if(isset($_COOKIE["MedusaToken"])) {
+        if(!isset($_GET['error'])){
+            if(isset($_COOKIE["MedusaToken"])) {
+                header('Location: auth.php?loginType=medusa');
+            }
+        }
+    } else {
+        error("Your request didn't deliver any useable Login-Tokens");
+    }
+
+    //Error aus Überprüfung ausgeben
+    if(isset($_GET['error'])){
+        if ($_GET['error'] == "userNotFound"){
+            error("You Key doesn't exist in our database");
+            die();
+        }
+        if ($_GET['error'] == "userDeleted"){
+            error("Your Account has been deleted");
+            die();
+        }
+        if ($_GET['error'] == "noType"){
+            error("Your request didn't deliver any Login-Type");
+            die();
+        }
+
     }
 
 ?>
