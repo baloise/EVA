493b2d090562a162519c039955817ff0f11f794e
Elia Reutlinger <elia.reutlinger@gibmit.ch>
2018-03-22 14:59:25 +0100

    Changed Drop

--- database/drop.php
+++ database/drop.php
@@ -3,10 +3,6 @@
     include('connect.php');
     include('../includes/session.php');
 
-    if($session_username != 'b000001'){
-        die('Permission denied');
-    }
-
     $sql = "DROP DATABASE db_eva";
 
     $mysqli->query($sql);
