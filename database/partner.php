a69fbe1ed63dc8f1c9db71d3263720dddbc50449
Elia Reutlinger <elia.reutlinger@gibmit.ch>
2018-03-26 13:37:21 +0200

    Small change

--- database/partner.php
+++ database/partner.php
@@ -1,18 +1,7 @@
 <?php
 
-    function formatAddress($person){
-        return  trim ("{$person['firstname']} {$person['lastname']}\n{$person['street']}\n{$person['city']}");
-    }
-
     function loadPerson($bkey) {
-        return array(
-            "firstname" => 'Maxli',
-            "lastname" => 'Mustermann',
-            "street" => 'Lange Strasse 354',
-            "city" =>  'CH-4002 Basel',
-            "email" => 'mail@test.com',
-            "gender" => '1'
-        );
+        return false;
     }
 
 ?>
