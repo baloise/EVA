4a72ae40ad53640e30194f5bcb858b08a020b0a3
Elia Reutlinger <elia.reutlinger@gibmit.ch>
2018-03-27 15:24:25 +0200

    small change

--- js/index.js
+++ js/index.js
@@ -25,7 +25,7 @@ function goBack(href){
             if ( status == "error" ) {
                 $('.loadScreen').fadeTo("fast", 0);
                 var msg = "makeDynamic Error";
-                alert( msg + xhr.status + " " + xhr.statusText );
+                comsole.log( msg + xhr.status + " " + xhr.statusText );
                 window.location.replace("logout.php");
             } else {
                 clearTimeout(loader);
