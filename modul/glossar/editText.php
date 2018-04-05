07bcbd0cb247211037f456f3d400d502ece9bbbc
Elia Reutlinger <elia.reutlinger@gibmit.ch>
2018-04-05 10:44:57 +0200

    Security Fix & finished #113

--- modul/glossar/editText.php
+++ modul/glossar/editText.php
@@ -7,8 +7,15 @@
         die($translate[145]);
     }
 
-    $newText = $_POST['newText'];
-    $lang = $_POST['textId'];
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+    $newText = test_input($_POST['newText']);
+    $lang = test_input($_POST['textId']);
 
     if($lang == 'ger'){
         $stmt = $mysqli->prepare("UPDATE `tb_text` SET de = ? WHERE `tb_text`.`type` = 'glossar';");
