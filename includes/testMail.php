ecdffe3c6f274446a146f10c268a4123bfde6200
Matthias Cullmann <matthias.cullmann@gmail.com>
2018-04-09 16:28:29 +0200

    Update testMail.php

--- includes/testMail.php
+++ includes/testMail.php
@@ -3,7 +3,7 @@
     ini_set('display_errors', 1);
 
     $t = time();
-    mail($_GET['to'], 'Test-E-Mail', $t, 'FROM: test@eva.ch');
+    mail($_GET['to'], 'Test-E-Mail', $t, 'FROM: b028178@baloise.ch');
     echo "Sent email with timestamp ".$t;
     
 ?>
