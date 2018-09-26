f0791b3664deb68d0a92753071b5df1e5114c75f
Elia Reutlinger <elia.reutlinger@gibmit.ch>
2018-09-26 16:13:50 +0200

    Fixed IE Problems for grades

--- grades/js/index.js
+++ grades/js/index.js
@@ -47,51 +47,41 @@ function makeSubject(array){
 function processXML(content){
 
     var htmlContent = "";
-
-    console.log(content);
-
     var array = JSON.parse("[" + content + "]");
-
     var array = array[0];
-    console.log(array);
-
     htmlContent = '<h2>' + array["Title"] + '</h2> <p> Created:' + array["Date"] + '</p>';
 
     for (var i in array) {
-
         for (var y in array[i]) {
-
             for (var z in array[i][y]) {
-
                 if(z == "Semester"){
 
-                    htmlContent = htmlContent + (`
-                        <div class="highlighter card"  style="padding: 20px;margin-top: 5px; margin-left:auto; margin-right:auto;">
-                            <div class="row" style="cursor: pointer;" onclick="toggleConts(` + array[i][y][z] + `)";>
-                                <div class="col-12 text-center">
-                                    <h2>Semester ` + array[i][y][z] + `</h2>
-                                </div>
-                            </div>
-                            <br/>
-
-                            <div class="row toToggleConts" contsID="` + array[i][y][z] + `" style="display:none;">
-                                <div class="col-lg-11" style=" margin-left: auto; margin-right: auto;">
-                    `);
+                    htmlContent = htmlContent + ('\
+                        <div class="highlighter card"  style="padding: 20px;margin-top: 5px; margin-left:auto; margin-right:auto;"> \
+                            <div class="row" style="cursor: pointer;" onclick="toggleConts(' + array[i][y][z] + ')";>\
+                                <div class="col-12 text-center">\
+                                    <h2>Semester ' + array[i][y][z] + '</h2>\
+                                </div>\
+                            </div>\
+                            <br/>\
+\
+                            <div class="row toToggleConts" contsID="' + array[i][y][z] + '" style="display:none;">\
+                                <div class="col-lg-11" style=" margin-left: auto; margin-right: auto;">\
+                    ');
 
                 } else  if(z== "Subjects"){
 
                     for (var x in array[i][y][z]) {
-                        //htmlContent = htmlContent + (z + " Entry: " + array[i][y][z][x] + "<br/>");
                         var subjectEntry = array[i][y][z][x];
                         htmlContent = htmlContent + makeSubject(subjectEntry);
                     }
 
-                    htmlContent = htmlContent + (`
-
-                                </div>
-                            </div>
-                        </div>
-                    `);
+                    htmlContent = htmlContent + ('\
+\
+                                </div>\
+                            </div>\
+                        </div>\
+                    ');
 
                 }
             }
