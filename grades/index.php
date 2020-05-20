60eecb9cb14001343a013e2e08f554698ea85717
Elia <mail@eliareutlinger.ch>
2020-05-20 16:15:02 +0200

    Update index.php

--- grades/index.php
+++ grades/index.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="description" content="View your grades created by Eva">
-        <meta name="author" content="Elia Reutlinger">
+        <meta name="author" content="Erleiuat">
 
         <title>Eva-Grades</title>
 
