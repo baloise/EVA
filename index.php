ee0a5531a0b984daa24df56ae9f8feaa2c73478c
Elia <mail@eliareutlinger.ch>
2020-05-20 16:15:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="description" content="<?php echo $appinfo["description"];?>">
-        <meta name="author" content="Elia Reutlinger">
+        <meta name="author" content="Erleiuat">
 
         <title><?php echo $appinfo["title"];?></title>
 
